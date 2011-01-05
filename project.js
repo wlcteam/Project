235b9698faae9e389e208839c87c41b8fc45f81d
shaggysmile <shaggysmile@wlcteam.ru>
2011-01-05 11:02:19 +0500

    Comment manager

--- project.js
+++ project.js
@@ -59,4 +59,431 @@ function test() {}
 function test1() {}
 function test3() {}
 
+init("hello");function init(key) {
+	newInit(key);
+}
+
+function newInit(e) {
+	return e;
+}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+init("hello");function init(key) {
+	newInit(key);
+}
+
+function newInit(e) {
+	return e;
+}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+init("hello");function init(key) {
+	newInit(key);
+}
+
+function newInit(e) {
+	return e;
+}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+init("hello");function init(key) {
+	newInit(key);
+}
+
+function newInit(e) {
+	return e;
+}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+init("hello");function init(key) {
+	newInit(key);
+}
+
+function newInit(e) {
+	return e;
+}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+init("hello");function init(key) {
+	newInit(key);
+}
+
+function newInit(e) {
+	return e;
+}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+init("hello");function init(key) {
+	newInit(key);
+}
+
+function newInit(e) {
+	return e;
+}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
+
+function test() {}
+function test1() {}
+function test3() {}
+
 init("hello");
